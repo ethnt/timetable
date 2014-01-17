5fbb039b91e7bd3552c88561121c915c72d8d70e
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-01-16 23:43:33 -0500

    initial commit.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+gem 'mongoid', '4.0.0.alpha2'
+
+gem 'turbolinks'
+
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeable::Application.config.secret_key_base = '6e62289f206658fec43d9ed408a03736e058c477d16b346b14706a5705a3168fa7d83dacda36efda1b2965d0620ccef5012b8e3e7bec56597d4c477faaefd5ab'
